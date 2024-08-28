0d33cc6feb5d58a936861f2aa364403d4e30397c
laurareded <lauramelo2308@hotmail.com>
2024-08-28 11:28:21 -0300

    API - alteracao aula 28-08

--- API/Projeto1/Program.cs
+++ API/Projeto1/Program.cs
@@ -1,6 +1,12 @@
+//Testar as APIs 
+//Rest client(extensao do vs), postman, insomnia 
+// MINIMAL APIs
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+
+//EndPoints - Funcionalidades
+//Configurar a URL e o método/verbo
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
--- API/api/Program.cs
+++ API/api/Program.cs
@@ -3,4 +3,18 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapGet("/segundafuncionalidade", () => "Hello mona");
+
+app.MapGet("/retornaendereco", () => 
+{
+    dynamic endereco = new {
+        rua = "Parigot de Souza",
+        numero = 5300,
+        cep = "81290-350"
+    };
+    return endereco;
+});
+
+
+
 app.Run();
